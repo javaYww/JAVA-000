65a7986c3299d6836b920d2ac95c3deb290a8b2d
muyi <java_yww@163.com>
2020-10-17 16:22:30 +0800

    week01-1 作业提交

---
+++ Week_01/Main.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main extends ClassLoader {
+
+	private static String filePath = "Hello.xlass";
+
+	private static String fileName = "Hello";
+
+	private static String methodName = "hello";
+
+	public static void main(String[] args) throws Exception {
+		Main main = new Main();
+		Class<?> clazz = main.loadClass(fileName);
+		Object hello = clazz.newInstance();
+		Method method = clazz.getDeclaredMethod(methodName);
+		method.invoke(hello);
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		try {
+			Path path = Paths.get(String.valueOf(new URL(filePath)));
+			byte[] bytes = Files.readAllBytes(path);
+			for (int i = 0; i < bytes.length; i++) {
+				bytes[i] = (byte) (255 - bytes[i]);
+			}
+			return defineClass(fileName, bytes, 0, bytes.length);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
