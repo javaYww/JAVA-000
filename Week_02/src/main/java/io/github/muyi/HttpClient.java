bbf5b63baae8f1667f62c3e8e5f1fad618dfcc4a
muyi <java_yww@163.com>
2020-10-26 11:32:00 +0800

    week02 作业提交

---
+++ Week_02/src/main/java/io/github/muyi/HttpClient.java
@@ -0,0 +1,48 @@
+package io.github.muyi;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClient {
+	private static final String URL = "http://localhost:8801";
+
+	public static void main(String[] args) {
+		httpClientGet(URL);
+		okHttpGet(URL);
+	}
+
+	private static void okHttpGet(String url) {
+		OkHttpClient okHttpClient = new OkHttpClient();
+		Request request = new Request
+				.Builder()
+				.url(url)
+				.build();
+		Call call = okHttpClient.newCall(request);
+		try(Response response = call.execute();) {
+			System.out.println(response.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void httpClientGet(String url) {
+		HttpGet httpGet = new HttpGet(url);
+		try (CloseableHttpClient client = HttpClientBuilder.create().build();
+		     CloseableHttpResponse response = client.execute(httpGet)) {
+			HttpEntity entity = response.getEntity();
+			System.out.println(EntityUtils.toString(entity));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
